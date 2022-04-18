9f8a18cafefa606752e801edb8a264ed25e02a03
SurazGaire <luxheartsuraz@gmail.com>
2022-04-18 12:16:17 +0545

    first commit

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get("/api/persons/:id", (request, response) => {
 		response.status(404).end();
 	}
 });
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
